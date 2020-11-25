f09eea5e298a2449f1f3cb2ae1043b0e02456679
murerr <rudy.murer.pro@gmail.com>
2020-11-25 17:23:10 +0100

    Removed npm l18n package

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,18 +1,9 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import VueI18n from 'vue-i18n'
 
 Vue.use(Vuetify);
-Vue.use(VueI18n)
-
-import fr from 'vuetify/es5/locale/fr'
-import en from "vuetify/es5/locale/en";
 
 export default new Vuetify({
-	lang: {
-		locales: { en, fr },
-		current: 'en',
-	},
 	icons: {
 		iconfont: 'mdiSvg', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
 	},

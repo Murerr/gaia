ad601078b2ad4b44eb32e24fb1126edfab7349c3
murerr <rudy.murer.pro@gmail.com>
2020-12-29 18:44:33 +0100

    adding french translation to home page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -18,41 +18,40 @@
 			</v-row>
 		</section>
 		<section id="about-me">
-			<div class="py-12"></div>
+			<div class="py-6"></div>
 				<v-container class="text-center">
-					<h2 class="display-2 font-weight-bold mb-3">Hi! I'm Rudy</h2>
-					<v-responsive class="mx-auto mb-8" width="56">
+					<h2 class="display-2 font-weight-bold">{{ $t('home.introduction') }}</h2>
+					<v-responsive class="mx-auto mb-6" width="56">
 						<v-divider class="mb-1"></v-divider>
 						<v-divider></v-divider>
 					</v-responsive>
-					<v-avatar class="elevation-12 mb-12" size="128">
+					<v-avatar class="elevation-12 mb-10" size="128">
 						<v-img src="./../assets/rudy_avatar.jpeg"></v-img>
 					</v-avatar>
-					<v-responsive class="mx-auto title font-weight-light mb-8 text-center" max-width="720">
-						Welcome to my website!
+					<v-responsive class="mx-auto title font-weight-light mb-6 text-center" max-width="720">
+            {{ $t('home.greetings') }}
 					</v-responsive>
 					<v-responsive class="mx-auto title font-weight-light text-justify" max-width="720">
-						22 years old Frenchman, I have graduated with a Bachelor of Computing and I have been working in Ireland for more than 2 years now.
-						I always have been passionate about new technologies, particularly mobile and web development. More recently, I have been interested in AI centered around tabletop games.<br><br>
+            {{ $t('home.whoami') }}<br><br>
 					</v-responsive>
 					<v-responsive class="mx-auto title font-weight-light mb-8 text-center" max-width="720">
-						You can have a look at the projects I'm working on by looking at my portfolio !
+            {{ $t('home.have-a-look') }}
 					</v-responsive>
 					<router-link class="porfolio-router-link pb-8" :to="{path: '/portfolio' }">
 					<v-btn color="primary" outlined large>
 						<span class="primary--text font-weight-bold">
-							My portfolio
+							{{ $t('home.my-portfolio') }}
 						</span>
 					</v-btn>
 					</router-link>
 					<v-responsive class="mx-auto title font-weight-light mt-8 text-center" max-width="720">
-						Do you want to know more about me and my career ? <br><br>
+            {{ $t('home.know-more') }} <br><br>
 					</v-responsive>
 
 					<router-link class="porfolio-router-link" :to="{path: '/about' }">
 						<v-btn color="primary" outlined large>
 						<span class="primary--text font-weight-bold">
-							About Me
+							{{ $t('home.about-me') }}
 						</span>
 						</v-btn>
 					</router-link>
